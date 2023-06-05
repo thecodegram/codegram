908b0304a5faa60ae97506de3feaa6b5082a99a2
dannyl1u <dannyjialiliu@gmail.com>
2023-06-04 22:33:44 -0700

    cron job

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,63 @@
-import express, { Request, Response } from 'express';
+const express = require('express');
+const axios = require('axios');
+const { CronJob } = require('cron');
 
+// Create an Express.js app
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
+// Define the list of user IDs
+const userIds: any[] = []; 
+
+// Function to make GraphQL requests for a specific user ID
+async function makeGraphQLRequest(userId) {
+  try {
+    // Make the GraphQL request
+    const response = await axios.post('https://leetcode.com/graphql', {
+      query: `
+      { 
+        matchedUser(username: "${userId}") 
+        {
+            username
+            submitStats: submitStatsGlobal 
+            {
+                acSubmissionNum 
+                {
+                    difficulty
+                    count
+                    submissions
+                }
+            }
+        }
+    }
+      `
+    });
+
+    // Handle the response
+    console.log(`User ID: ${userId}`);
+    console.log(response.data.data.matchedUser);
+  } catch (error) {
+    console.error(`Error fetching data for user ID ${userId}:`, error);
+  }
+}
+
+// Set up the cron job to run every 2 minutes
+const job = new CronJob('*/2 * * * *', () => {
+  for (const userId of userIds) {
+    makeGraphQLRequest(userId);
+  }
+});
+job.start();
+
+// Define an endpoint to trigger the GraphQL requests manually
+app.get('/trigger-requests/:userId', (req, res) => {
+  const { userId } = req.params;
+  userIds.push(userId);
+  // makeGraphQLRequest(userId);
+  res.send(`GraphQL request triggered for User ID: ${userId}`);
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
