eddd2d715260cadb513fde8218ff815828180861
George Shramko <shramko.georgiy@gmail.com>
2023-06-14 16:22:56 -0700

    Refactoring
    - Added cron @types lib
    Code  was not compiling for me without it. 2.0.1 is latest
    - Using import instead of require. (Compile time vs runtime)
    - Added single user endpoint trigger
    - UserIds are a set now to remove duplciates
    - Multiple requests to leetcode are sent in parallel
    
    Jira: gotta-create-one

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
-import { json } from "express";
-
-const express = require('express');
-const axios = require('axios');
-const { CronJob } = require('cron');
+import express, { Request, Response } from 'express';
+import axios from 'axios';
+import { CronJob } from 'cron';
 
 // Create an Express.js app
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Define the list of user IDs
-const userIds: any[] = []; 
+const userIds = new Set<string>(); 
 
 // Function to make GraphQL requests for a specific user ID
-async function makeGraphQLRequest(userId: any) {
+async function makeGraphQLRequest(userId: String) {
   try {
     // Make the GraphQL request
     const response = await axios.post('https://leetcode.com/graphql', {
@@ -45,25 +43,55 @@ async function makeGraphQLRequest(userId: any) {
 }
 
 // Set up the cron job to run every 2 minutes
-// const job = new CronJob('*/2 * * * *', () => {
-//   for (const userId of userIds) {
-//     makeGraphQLRequest(userId);
-//   }
-// });
+const job = new CronJob('*/2 * * * *', () => {
+  for (const userId of userIds) {
+    makeGraphQLRequest(userId);
+  }
+});
 // job.start();
 
+// Add userId to tracking
+app.post('/api/addUser/:userId', (req: Request, res: Response ) => {
+  const {userId} = req.params;
+
+  if(userIds.has(userId)){
+    res.status(201).send("Already exists")
+    return
+  }
+
+  userIds.add(userId)
+  res.sendStatus(200)
+})
+
 // Define an endpoint to trigger the GraphQL requests manually
-app.get('/trigger-requests/:userId', async (req: any, res: any ) => {
-  const { userId } = req.params;
-  userIds.push(userId);
-  // makeGraphQLRequest(userId);
-  // res.send(`GraphQL request triggered for User ID: ${userId}`);
-  // console.log(makeGraphQLRequest(userId));
-  const data = await makeGraphQLRequest(userId);
+app.get('/api/trigger-requests/', async (req: any, res: any ) => {
+  const promises: Promise<ReturnType<typeof makeGraphQLRequest>>[] = 
+    Array.from(userIds).map(async (id) => {
+      const cur = await makeGraphQLRequest(id);
+      console.log("cur:", cur);
+      return cur;
+    });
+  
+  const data = await Promise.all(promises);
+  console.log(data);
+  
   res.send(data);
 });
+
+// trigger for specific user
+app.get('/api/trigger-requests/:userId', async (req: any, res: any ) => {
+  const { userId } = req.params;
+  
+  const data = await makeGraphQLRequest(userId);
+  console.log("data:", data);
+  
+  res.send(data);
+});
+
+
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  console.log(`Navigate to http://localhost:${port}/`)
 });
