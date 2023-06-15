2f7d0d63df13ba7654c3591ae5b228a5c3c8ae17
George Shramko <shramko.georgiy@gmail.com>
2023-06-14 20:41:51 -0700

    Fixing security error, refactor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,27 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
 import { CronJob } from 'cron';
+import { param, validationResult } from 'express-validator'
 
 // Create an Express.js app
 const app = express();
 const port = 8080;
 
 // Define the list of user IDs
-const userIds = new Set<string>(); 
+const userIds = new Set<string>();
+const allowedUsername: RegExp = /^[a-zA-Z0-9_-]+$/
 
+const handleValidationErrors = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send("Bad request");
+  }
+  next();
+};
 // Function to make GraphQL requests for a specific user ID
 async function makeGraphQLRequest(userId: String) {
   try {
@@ -51,10 +64,14 @@ const job = new CronJob('*/2 * * * *', () => {
 // job.start();
 
 // Add userId to tracking
-app.post('/api/addUser/:userId', (req: Request, res: Response ) => {
-  const {userId} = req.params;
+app.post('/api/addUser/:userId', [
+  // Sanitize the userId variable
+  param('userId').escape().matches(allowedUsername),
+  handleValidationErrors
+], (req: Request, res: Response) => {
+  const { userId } = req.params;
 
-  if(userIds.has(userId)){
+  if (userIds.has(userId)) {
     res.status(201).send("Already exists")
     return
   }
@@ -64,27 +81,32 @@ app.post('/api/addUser/:userId', (req: Request, res: Response ) => {
 })
 
 // Define an endpoint to trigger the GraphQL requests manually
-app.get('/api/trigger-requests/', async (req: Request, res: Response ) => {
-  const promises: Promise<ReturnType<typeof makeGraphQLRequest>>[] = 
+app.get('/api/trigger-requests/', async (req: Request, res: Response) => {
+  const promises: Promise<ReturnType<typeof makeGraphQLRequest>>[] =
     Array.from(userIds).map(async (id) => {
       const cur = await makeGraphQLRequest(id);
       console.log("cur:", cur);
       return cur;
     });
-  
+
   const data = await Promise.all(promises);
   console.log(data);
-  
+
   res.send(data);
 });
 
 // trigger for specific user
-app.get('/api/trigger-requests/:userId', async (req: Request, res: Response ) => {
+app.get('/api/trigger-requests/:userId', [
+  // Sanitize the userId variable
+  param('userId').escape().matches(allowedUsername),
+  handleValidationErrors
+], async (req: Request, res: Response) => {
   const { userId } = req.params;
-  
+
+
   const data = await makeGraphQLRequest(userId);
   console.log("data:", data);
-  
+
   res.send(data);
 });
 
