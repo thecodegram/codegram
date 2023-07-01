967b159bb2e78f633f229c20cabb27492bbb4767
PEYZ <zhiani2000@gmail.com>
2023-06-30 23:01:56 -0700

    temp

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import styles from "./LoginPage.module.css"
+import "./LoginPage.css"
+
+const LoginPage = () => {
+  return (
+    <React.Fragment>
+      <div className="container-center-horizontal">
+        Hello
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default LoginPage;
