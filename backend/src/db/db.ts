dae8471fd09edb531a19b72d15077ab25789bb69
George Shramko <shramko.georgiy@gmail.com>
2023-07-12 12:26:11 -0700

    Setting up postgres db

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,20 +1,52 @@
-import mongoose from 'mongoose';
 import { env } from '../config/env';
+import mongoose from 'mongoose';
+import {Pool} from 'pg'
+import fs from 'fs'
+import path from 'path'
 
+async function connectToMongo() {
+  try {
+    const MONGO_CONNECTION_STRING = env.MONGODB_CONNECTION_STRING!!;
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(MONGO_CONNECTION_STRING, {
+      maxConnecting: 30
+    });
 
+    
+    return true;
+    
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+    return false;
+  }
+}
+
+const PSQL_CONNECTION_STRING = env.PSQL_CONNECTION_STRING;
+export const pool = new Pool({connectionString: PSQL_CONNECTION_STRING});
+
+async function connectToPostgresAndCreateTables(){
+  try {
+    const dbInitQuery = await fs.promises.readFile(path.join(__dirname,'db_init.sql'), 'utf8');
+    await pool.query(dbInitQuery);
+
+    console.log('Connected to the database!');
+  } catch(e) {
+    console.log("Encountered error during db init");
+    console.error(e);
+    return false;
+  }
+
+  return true;
+}
 export async function setUpDB(){
-    try {
-        const DB_CONNECTION_STRING = env.MONGODB_CONNECTION_STRING!!;
-        mongoose.set('strictQuery', false);
-        await mongoose.connect(DB_CONNECTION_STRING, {
-          maxConnecting: 30
-        });
-        console.log('Connected to the database!');
-        
-        return true;
-        
-      } catch (error) {
-        console.error('Failed to connect to MongoDB:', error);
-        return false;
-      }
+    const isConnectedToMongo = await connectToMongo();
+    const isConnectedToPostgres = await connectToPostgresAndCreateTables();
+
+    
+    if(isConnectedToMongo && isConnectedToPostgres) {
+      console.log('Connected to the database!');
+      return true;
+    }
+
+    return false;
 }
