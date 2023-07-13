79df41b8cc8d8191244f006e48660c6e72dc42a6
George Shramko <shramko.georgiy@gmail.com>
2023-07-12 18:10:59 -0700

    Endpoint for linking vjudge/leetcode
    - PUT /api/user/<username> with {leetcodeUsername:"..." and/or vjudgeUsername:"..."}
      updates the username in leetcode and/or vjudge. If user not found returns 404 and doesn't update
      can only update yourself (session.username has to match request's username)
      after updating queries leetcode/vjudge for submission stats and updates the db
    
    - GET /api/user/<username>
      get all submission data (in the db) for the user
      requester has to be logged in, but username doesnt have to match
    
    - Updated the schemas so data from api's is stored seamlessly

--- backend/src/routes/users/users-route.ts
+++ backend/src/routes/users/users-route.ts
@@ -2,6 +2,10 @@ import express, { Request, Response } from 'express'
 
 import { validateUsername, handleValidationErrors } from '../../utils/middleware'
 import { getUserIDs, addUserID } from '../../model/users';
+import { User } from '../../model/schemas/userSchema';
+import { UserNameNotFoundError } from '../../errors/username-not-found-error';
+import { getSubmissionStats } from '../../api/vjudge';
+import { makeGraphQLRequest } from '../../api/leetcode';
 
 const router = express.Router()
 
@@ -10,6 +14,8 @@ router.post('/:userId', [
   validateUsername('userId'),
   handleValidationErrors
 ], (req: Request, res: Response) => {
+  const data = req.body;
+  
   const { userId } = req.params;
   const userIDs = getUserIDs();
 
@@ -20,6 +26,103 @@ router.post('/:userId', [
 
   addUserID(userId)
   res.sendStatus(200)
+})
+
+router.put('/:username', [
+  // Sanitize the userId variable
+  validateUsername('username'),
+  handleValidationErrors
+], async (req: Request, res: Response) => {
+  const {username} = req.params;
+  
+  const sessionUsername = req.session.username;
+
+  // need to be logged in as that user to mmodify it
+  if(username != sessionUsername){
+    res.status(403).send("Forbidden");
+  } else {
+    const data = req.body;
+
+    if(!data) {
+      res.send(400);
+    }
+    else {
+      // find the entry in the db for this user
+      const user = await User.findOne(
+        {username: username},
+        {
+          password: false
+        });
+      
+      if(!user) {
+        res.status(500).send('Authorized user was not found in the db')
+      } else {
+        try {
+          // if update request has a new vjudge name
+          // ensure this is a valid name
+          // update the vjudge username and latest data
+          const updateVjudge = (async () => {
+            if(data.vjudgeUsername) {
+                const vjudgeStats = await getSubmissionStats(data.vjudgeUsername);
+                console.log(vjudgeStats);
+                vjudgeStats.username = data.vjudgeUsername;
+                user.vjudge = vjudgeStats;
+            }
+          })();
+          // if update request has a new vjudge name
+          // ensure this is a valid name
+          // update the vjudge username and latest data
+          const updateLeetcode = (async() => {if(data.leetcodeUsername) {
+            const leetcodeStats = await makeGraphQLRequest(data.leetcodeUsername);
+            user.leetcode = leetcodeStats;
+          }})();
+          
+          // update in parallel
+          await updateVjudge;
+          await updateLeetcode;
+
+          user.save();
+
+          res.sendStatus(200);
+        } catch(err) {
+          if(err instanceof UserNameNotFoundError){
+            res.status(404).send(err.message);
+          }
+          else {
+            console.error(err);
+            res.status(500).send("Something went wrong");
+          }
+        }
+      }
+    }
+  }
+})
+
+router.get('/:userId', [
+  // Sanitize the userId variable
+  validateUsername('userId'),
+  handleValidationErrors
+], async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  
+  const userData = await User.findOne(
+    {username: userId},
+    {
+      password: false,
+      'leetcode._id':false,
+      'leetcode.submitStats._id':false,
+      'leetcode.submitStats.acSubmissionNum._id':false
+    });
+
+  if(!userData){
+    res.status(404).send("User not found");
+  }
+  else {
+    console.log(userData);
+
+    addUserID(userId)
+    res.status(200).json(userData);
+  }
 })
 
 module.exports = router;
--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -1,4 +1,4 @@
 export const validUsername: RegExp = /^[a-zA-Z0-9_-]+$/
 
-export const isValidUsername = (username: String) => validUsername.test(username.toString())
+export const isValidUsername = (username: String) => validUsername.test(username?.toString())
 
