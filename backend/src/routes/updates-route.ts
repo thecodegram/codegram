f330f6711e6e93986824b7cef716af6d471ac368
PEYZ <zhiani2000@gmail.com>
2023-07-29 01:08:17 -0700

    added a test endpoint to test it with the users in db

--- backend/src/routes/updates-route.ts
+++ backend/src/routes/updates-route.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import updateRankingsJob from "../job/RankingJob";
+import { pool } from "../db/db";
 
 const router = express.Router();
 
@@ -20,5 +22,33 @@ router.get("/", [], async (req: Request, res: Response) => {
 
 });
 
+////TESTING ONLY
+router.get(
+  "/testUpdateRankings",
+  async (req: Request, res: Response) => {
+    try {
+      await updateRankingsJob();
+      res.status(200).send('Rankings updated successfully.');
+    } catch (err) {
+      console.error('An error occurred:', err);
+      res.status(500).send('An error occurred while updating the rankings.');
+    }
+  }
+);
+
+////TESTING ONLY
+router.get(
+  "/getAllRanks",
+  async (req: Request, res: Response) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM users ORDER BY current_rank ASC");
+      res.status(200).json(result.rows);
+    } catch (err) {
+      console.error('An error occurred:', err);
+      res.status(500).send('An error occurred while getting the rankings.');
+    }
+  }
+);
 
 module.exports = router;
