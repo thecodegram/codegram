6b53ca6b676d49347d7ce6c41f7ae4b8eb4dbf8d
George Shramko <shramko.georgiy@gmail.com>
2023-07-31 03:04:31 -0700

    Introduce batches to scheduler to reduce load on external api

--- backend/src/scheduler/scheduler.ts
+++ backend/src/scheduler/scheduler.ts
@@ -11,22 +11,53 @@ export class Scheduler {
   private readonly leetcodeUpdatesCollectorJob = new CronJob('* * * * *', async () => {
 
     const userData = await this.userRepository.getAllUsernames();
+    
+    // we are querying external APIs, so we cannot do any load we want
+    // Introduce batches of size <= 10 that we will process once in 0.2 seconds;
+    const batchSize = 10;
+    const delayBetweenBatches = 200;
+    const totalBatches = Math.ceil(userData.length / batchSize);
 
-    await Promise.all(userData.map(u => {
-      // should probably change this to use id
-      getAndStoreLeetcodeUpdates(u.username!!)
-    }))
+  for (let i = 0; i < totalBatches; i++) {
+    const startIndex = i * batchSize;
+    const endIndex = Math.min(startIndex + batchSize, userData.length);
+    const batch = userData.slice(startIndex, endIndex);
+
+    // Process the current batch of users concurrently using Promise.all
+    await Promise.all(batch.map(u => getAndStoreLeetcodeUpdates(u.username!!)));
+
+    // Introduce a delay between batches (in milliseconds)
+    if (i < totalBatches - 1) {
+      await new Promise(resolve => setTimeout(resolve, delayBetweenBatches));
+    }
+  }
 
   });
 
   private readonly vjudgeUpdatesCollectorJob = new CronJob('*/5 * * * *', async () => {
 
     const userData = await this.userRepository.getAllUsernames();
+    
+    // we are querying external APIs, so we cannot do any load we want
+    // VJudge is most likely not as robust as LeetCode so reduce the load compared to Leetcode
+    // Introduce batches of size <= 5 that we will process once in 0.5 seconds;
+    const batchSize = 5;
+    const delayBetweenBatches = 200;
+    const totalBatches = Math.ceil(userData.length / batchSize);
 
-    await Promise.all(userData.map(u => {
-      // should probably change this to use id
-      getAndStoreVjudgeUpdates(u.username!!)
-    }))
+  for (let i = 0; i < totalBatches; i++) {
+    const startIndex = i * batchSize;
+    const endIndex = Math.min(startIndex + batchSize, userData.length);
+    const batch = userData.slice(startIndex, endIndex);
+
+    // Process the current batch of users concurrently
+    await Promise.all(batch.map(u => getAndStoreVjudgeUpdates(u.username!!)));
+
+    // Wait <delay> time before processing next batch;
+    if (i < totalBatches - 1) {
+      await new Promise(resolve => setTimeout(resolve, delayBetweenBatches));
+    }
+  }
 
   });
 
