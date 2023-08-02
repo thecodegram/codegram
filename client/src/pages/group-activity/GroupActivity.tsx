3f21c3f8b2d9754a3484b1ae17c77471be263ba9
Kiaan Castillo <kiaan_castillo@sfu.ca>
2023-08-01 22:14:18 -0700

    Add lazy loading to group activity feed

--- client/src/pages/group-activity/GroupActivity.tsx
+++ client/src/pages/group-activity/GroupActivity.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { EmptyState } from "../../components/empty-state/EmptyState"
 import { FeedItem } from "../../components/feed-item/FeedItem"
 
@@ -20,12 +20,33 @@ interface ActivityItem {
 
 export const GroupProfileActivity = () => {
   const { groupId } = useParams()
-  const [ activtyData, setActivityData ] = useState<ActivityItem[]>([])
+  const [ activityData, setActivityData ] = useState<ActivityItem[]>([])
+  const bottomOfFeedRef = useRef<HTMLDivElement>(null)
   const [ offset, setOffset ] = useState<number>(0)
-  const limit = 25
+  const [ loading, setLoading ] = useState<boolean>(false)
+  const [ isEndOfOffset, setIsEndOfOffset ] = useState<boolean>(false)
+  const [isDelayActive, setIsDelayActive] = useState<boolean>(false)
+  
+  useEffect(() => {
+    const delayMs: number = 1000
+    let delayTimer: NodeJS.Timeout | null = null
+
+    if (isDelayActive) {
+      delayTimer = setTimeout(() => setIsDelayActive(false), delayMs)
+    }
+
+    return () => {
+      if (delayTimer) {
+        clearTimeout(delayTimer)
+      }
+    }
+  }, [isDelayActive])
 
   useEffect(() => {
     const fetchData = async () => {
+      const limit: number = 25
+      setLoading(true)
+
       try {
         const response = await axios.get(
           `${process.env.REACT_APP_API_URL}/api/events/group?group_id=${groupId}&offset=${offset}&limit=${limit}`,
@@ -34,8 +55,14 @@ export const GroupProfileActivity = () => {
           }
         );
         const jsonData = await response.data;
+
+        if (jsonData && jsonData.length === 0) {
+          setIsEndOfOffset(true)
+          return
+        }
   
-        setActivityData(jsonData);
+        setActivityData([...activityData, ...jsonData]);
+        setLoading(false)
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -46,20 +73,42 @@ export const GroupProfileActivity = () => {
     }
   }, [groupId, offset]);
 
+  
+  useEffect(() => {
+    if (!bottomOfFeedRef.current) return
+
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    }
+
+    const scrollObserver = new IntersectionObserver((entries) => {
+      if (!isDelayActive && entries?.[0]?.isIntersecting && !loading && !isEndOfOffset) {
+        setOffset(() => offset + 1)
+        setIsDelayActive(true)
+      }
+    }, options);
+
+    scrollObserver.observe(bottomOfFeedRef.current);
+  })
+
   return <article className={styles.groupProfileActivity}>
-    {activtyData.length === 0 
+    {activityData.length === 0 
       ? <EmptyState>No activity yet</EmptyState>
-      : activtyData.map(({ username, platform, problemTitle, problemTitleSlug, likes, timestamp }, index) => <FeedItem 
-          key={index}
-          name={username}
-          username={username}
-          platform={platform}
-          problemTitle={problemTitle}
-          problemTitleSlug={problemTitleSlug}
-          numOfLikes={+likes}
-          createdTime={new Date(timestamp)}
-        />
-      )
+      : <>
+          {activityData.map(({ username, platform, problemTitle, problemTitleSlug, likes, timestamp }, index) => <FeedItem 
+            key={index}
+            name={username}
+            username={username}
+            platform={platform}
+            problemTitle={problemTitle}
+            problemTitleSlug={problemTitleSlug}
+            numOfLikes={+likes}
+            createdTime={new Date(timestamp)}
+          />)}
+          <div ref={bottomOfFeedRef} style={{ width: "100%", height: "1px" }}></div>
+      </>
     }
   </article>
 }
