3de6747bb06a748e877c54493ac99bac14675c8b
dannyl1u <dannyjialiliu@gmail.com>
2023-08-03 23:45:19 -0700

    update schema

--- backend/src/model/schemas/userSchema.ts
+++ backend/src/model/schemas/userSchema.ts
@@ -14,7 +14,7 @@ const userSchema = new Schema({
   },
   password: {
     type: String,
-    required: true,
+    required: false,
   },
   leetcode:  leetcodeStatsSchema,
   vjudge:  vjudgeStatsSchema,
