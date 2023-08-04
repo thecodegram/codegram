221dcf7186c72c934f9ed2ab93f46fb612850a13
George Shramko <shramko.georgiy@gmail.com>
2023-08-04 15:06:25 -0700

    Go to login after logout

--- client/src/components/header-nav/HeaderNav.tsx
+++ client/src/components/header-nav/HeaderNav.tsx
@@ -44,6 +44,7 @@ export const HeaderNav = () => {
         const auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(() => {
           console.log("User signed out of Google session");
+          navigate("/login");
         });
 
         navigate("/login");
