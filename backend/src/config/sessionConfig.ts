7140efc0b49921fa7a6b641084853da5950a879a
George Shramko <shramko.georgiy@gmail.com>
2023-08-31 17:10:52 -0700

    env-dependent cookie config

--- backend/src/config/sessionConfig.ts
+++ backend/src/config/sessionConfig.ts
@@ -2,6 +2,20 @@ import { env } from "./env";
 import {FirestoreStore} from "@google-cloud/connect-firestore";
 import { Firestore } from '@google-cloud/firestore';
 
+var cookieConfig;
+if (env.ENVIRONMENT === 'DEV') {
+    cookieConfig = {
+        maxAge: 3600000, // 1 hour in milliseconds
+    }
+} else {
+    cookieConfig = {
+        maxAge: 3600000, // 1 hour in milliseconds
+        sameSite: 'none' as 'none',
+        secure: true,
+        domain: 'backend2-c2h4n4r2vq-uw.a.run.app',
+    }
+}
+
 export const sessionOptions = {
     name: 'mysession',
     secret: env.AUTH_SECRET_KEY!!,
@@ -11,10 +25,5 @@ export const sessionOptions = {
         dataset: new Firestore(),
         kind: 'express-sessions',
       }),
-    cookie: {
-        maxAge: 3600000, // 1 hour in milliseconds
-        // sameSite: 'none' as 'none',
-        // secure: true,
-        // domain: 'backend2-c2h4n4r2vq-uw.a.run.app',
-    }
+    cookie: cookieConfig
 }
